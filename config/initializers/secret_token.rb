9b1f9b640cb28c3e0c92dd40432d346d206ad55f
Des Power <despower1@mac.com>
2013-09-25 12:06:36 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppFive::Application.config.secret_token = 'df4a553302fab2bb5ce034f2fb753ff247f2f75612fa4b2ebe3b06d75ef399ad4dbba3383754552576fca72cfa5c7ab8f4b0378ef3c7a1762d26f531572af707'
